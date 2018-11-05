4d47d24195a295e3a84b8587bf2d4c169479a3ac
Allan Laursen <ninja@ka-net.dk>
2018-11-05 11:47:08 +0100

    IC-2018.2.5 <Steve@Steve-OS Overwrite remote https://github.com/GudmundurSig/Auth.git to local

--- Printerface.java
+++
@@ -1,8 +0,0 @@
-package com.DTU;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Printerface extends Remote {
-    public String Echo(String input) throws RemoteException;
-}
